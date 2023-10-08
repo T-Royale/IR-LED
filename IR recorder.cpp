1fccd8714bb6036f2005337503c3a2e26aacee03
T_Royale <141929186+T-Royale@users.noreply.github.com>
2023-10-08 15:19:28 +0200

    V 2.1
    Updated to IrRemote 4.2.0

--- IR recorder.cpp
+++ IR recorder.cpp
@@ -1,18 +1,19 @@
 // You need to install the IRremote library before starting //
-#include <IRremote.hpp>
-
-const int receptorPin = A3;
-IRrecv receptor(receptorPin);
-decode_results resultados;
+#include <IRremote.h>
+// The signal pin on the receiver must be connected to the A3 pin //
+#define SIGNAL_PIN A3 
+IRrecv receiver(SIGNAL_PIN);
 
 void setup() {
   Serial.begin(9600);
-  receptor.enableIRIn();
+//enables ir data capture//
+  receiver.enableIRIn();
 }
-
+//When the receiver detects a signal, it will be shown on the serial monitor in the IDE//
 void loop() {
-  if (receptor.decode(&resultados)) {
-    Serial.println(resultados.value, HEX);
-    receptor.resume();
+  if (receiver.decode()) {
+    Serial.println(receiver.decodedIRData.decodedRawData, HEX);
+  //Restart the receiver. This is very important to capture the next signal
+    receiver.resume();
   }
 }
