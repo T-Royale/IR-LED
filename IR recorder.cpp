9ac147355237e1223fa5972c375f7eb76d7b8047
T_Royale <141929186+T-Royale@users.noreply.github.com>
2023-10-10 18:24:44 +0200

    V 2.3
    With this changes, I made the button's ID look much clearer.

--- IR recorder.cpp
+++ IR recorder.cpp
@@ -1,20 +1,23 @@
-// You need to install the IRremote library before starting //
 #include <IRremote.h>
-// The signal pin on the receiver must be connected to the A3 pin //
-#define SIGNAL_PIN A3 
+#define SIGNAL_PIN A3
+
+int ButtonCount = 1;
+String SignalID;
 IRrecv receiver(SIGNAL_PIN);
+String Message;
 
 void setup() {
   Serial.begin(9600);
-//enables ir data capture//
   receiver.enableIRIn();
 }
-//When the receiver detects a signal, it will be shown on the serial monitor in the IDE//
+
 void loop() {
   if (receiver.decode()) {
-    Serial.println(receiver.decodedIRData.decodedRawData, HEX);
-    delay(1000);
-  //Restart the receiver. This is very important to capture the next signal
+    SignalID = receiver.decodedIRData.decodedRawData, HEX;
+    Message = "Button" + String(ButtonCount) + "= " + String(SignalID);
+    Serial.println (Message);
+    ButtonCount ++;
+    delay(500);
     receiver.resume();
   }
 }
